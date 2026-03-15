369667f4fdaf41aa5be9beb0e5cbb1e756bab8d1
tanvir-wd22 <tanvirwd22@gmail.com>
2026-03-16 02:21:52 +0600

    alhamdulliah banner section done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,31 @@
+import google from '../assets/google.png';
+import apple from '../assets/apple.png';
+
+const Banner = () => {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-center font-bold text-2xl lg:text-4xl">
+        We Build <br />
+        <span className="text-purple-500 mr-2">Productive</span>
+        Apps
+      </h1>
+      <p className="text-center opacity-70">
+        At Tenor It we craft innovative apps designed to make everyday life
+        simpler, smarter, and more exciting. <br /> Our goal is to turn your
+        ideas into digital experiences that truly make an impact.
+      </p>
+      <div className="flex justify-center items-center gap-6 text-sm font-semibold">
+        <button className="flex items-center gap-1 btn btn-soft">
+          <img src={google} className="w-6" alt="google" />
+          <span>Play Store</span>
+        </button>
+        <button className="flex items-center gap-1 btn btn-soft">
+          <img src={apple} className="w-6" alt="apple" />
+          <span>App Store</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import Banner from '../components/Banner';
+import Hero from '../components/Hero';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <section className="mb-4 lg:mb-8 w-11/12 mx-auto">
+        <Banner></Banner>
+      </section>
+      <section>
+        <Hero></Hero>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
